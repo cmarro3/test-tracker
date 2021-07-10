b3d3abe08a0a457a993163a90c2b1268b1ccab53
cmarro3 <christophermarroquin352@gmail.com>
2021-07-10 13:03:06 -0500

    update one

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <!--v-on:click="onClick()" works the same as @click="onClick()"-->
+    <button @click="onClick()" :style="{background:color}" class="btn">
+        {{text}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String
+    },
+    methods:{
+        onclick(){
+            console.log('Click');
+        }
+    }
+}
+</script>
